5dee6935b12e5154d4efa4455491ae51d202407a
Tomislav Šuto <113673335+RetroApe@users.noreply.github.com>
2025-01-04 14:38:06 +0100

    .

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const reversePoly = document.querySelector('.reverse-polygon');
 
 function matchHeight() {
     const height = originalPoly.offsetHeight;
+    const width = window.getComputedStyle(originalPoly).width;
+
     reversePoly.style.height = `${height}px`;
-    console.log(`${height}px`);
-}
+    reversePoly.style.width = `${width}`;
+} 
 
 // Run on load and resize
 window.addEventListener('load', matchHeight);
