fee18a133e106dcc0a261ef802e8f8b9f62c4079
Tomislav Šuto <113673335+RetroApe@users.noreply.github.com>
2025-01-05 13:48:32 +0100

    .

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+'use strict';
+
+    // Load navigation
+fetch('nav.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('navigation').innerHTML = data;
+    });
+
+// Load footer
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer').innerHTML = data;
+    });
+
+
+
 const originalPoly = document.querySelector('.polygon');
 const reversePoly = document.querySelector('.reverse-polygon');
 
@@ -12,3 +30,21 @@ function matchHeight() {
 // Run on load and resize
 window.addEventListener('load', matchHeight);
 window.addEventListener('resize', matchHeight);
+
+
+
+
+
+var width1 = 1440;
+var width2 = 320;
+var clampMax = 32;
+var clampMin = 24;
+
+var clampX;
+var clampY;
+
+clampY = (clampMax - clampMin) / (width1 - width2);
+clampX = clampMax - width1 * clampY;
+
+console.log(`clamp(${clampMin}px, ${clampX.toFixed(4)}px + ${(clampY * 100).toFixed(4)}vw, ${clampMax}px);`);
+console.log(`clamp(${clampMin / 16}rem, ${(clampX / 16).toFixed(4)}rem + ${(clampY * 100).toFixed(4)}vw, ${clampMax / 16}rem);`);
